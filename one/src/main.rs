fe36a5d8b66cb95cc39280ce8f6b71a84d8ab0b8
Swann HERRERA <swann@devloup.dev>
2022-12-01 11:16:33 +0100

    exo 1

--- one/src/main.rs
+++ one/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("one/data.txt")?;
+    let mut buffer = BufReader::new(file);
+    let mut content = String::new();
+    let _read_to_stirng_result = buffer.read_to_string(&mut content)?;
+    let elve_inventories: Vec<&str> = content.split("\n\n").collect();
+
+    let sums: Vec<u32> = elve_inventories.into_iter().map(|inventory| {
+        inventory.lines().map(|s| s.parse::<u32>().expect("line is number")).sum()
+    }).collect();
+    let max = sums.into_iter().max_by(|a, b| a.cmp(b));
+
+    println!("{}", max.unwrap());
+
+    Ok(())
 }
---
+++ target/debug/deps/one-a6d47b57bccec680.d
@@ -0,0 +1,5 @@
+/Users/swann/private-repositories/github.com/swannHERRERA/advent-of-code-2022/target/debug/deps/one-a6d47b57bccec680.rmeta: one/src/main.rs
+
+/Users/swann/private-repositories/github.com/swannHERRERA/advent-of-code-2022/target/debug/deps/one-a6d47b57bccec680.d: one/src/main.rs
+
+one/src/main.rs:

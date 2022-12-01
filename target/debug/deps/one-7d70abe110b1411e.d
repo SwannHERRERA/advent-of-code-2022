e9ca8ba7534d8c0eb7b29646031fd1441fde79a2
Swann HERRERA <swann@devloup.dev>
2022-12-01 10:45:14 +0100

    change to use workspaces

---
+++ target/debug/deps/one-7d70abe110b1411e.d
@@ -0,0 +1,5 @@
+/Users/swann/private-repositories/github.com/swannHERRERA/advent-of-code-2022/target/debug/deps/one-7d70abe110b1411e: one/src/main.rs
+
+/Users/swann/private-repositories/github.com/swannHERRERA/advent-of-code-2022/target/debug/deps/one-7d70abe110b1411e.d: one/src/main.rs
+
+one/src/main.rs:
---
+++ target/debug/one.d
@@ -0,0 +1,1 @@
+/Users/swann/private-repositories/github.com/swannHERRERA/advent-of-code-2022/target/debug/one: /Users/swann/private-repositories/github.com/swannHERRERA/advent-of-code-2022/one/src/main.rs

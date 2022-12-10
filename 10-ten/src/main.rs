461f1f55c4be3189dac5cb592a98b9428ecb8729
Swann HERRERA <swann@devloup.dev>
2022-12-10 11:27:49 +0100

    day-10

---
+++ 10-ten/src/main.rs
@@ -0,0 +1,29 @@
+mod timer;
+use std::fs;
+use timer::*;
+
+fn parse_line(line:&str) -> Command {
+    if line == "noop" {
+        return Command::Noop;
+    }
+    let (_, value) = line.split_once(' ').unwrap();
+    let value: i64 = value.parse().unwrap();
+    Command::AddX(value)
+}
+fn parse_input(input: &str) -> Vec<Command> {
+    input.lines().map(parse_line).collect()
+}
+
+fn main() {
+    let input = fs::read_to_string("10-ten/input.txt").unwrap();
+    let commands = parse_input(&input);
+    let mut timer = Timer::new();
+    for command in commands {
+        timer.next_cycle(command);
+    }
+    let part_one = timer.get_sum();
+    println!("{:?}", timer);
+    println!("part one : {}", part_one);
+    let part_two = timer.get_ctr();
+    println!("{}", part_two);
+}
---
+++ 10-ten/src/timer.rs
@@ -0,0 +1,89 @@
+#[derive(Debug, Eq, PartialEq)]
+pub enum Command {
+    Noop,
+    AddX(i64),
+}
+
+#[derive(Debug)]
+pub struct Timer {
+    ctr: String,
+    current_cycle: usize,
+    value: i64,
+    next_value: Option<i64>,
+    signial_strengh_over_time: Vec<i64>,
+}
+
+impl Timer {
+    pub fn new() -> Self {
+        Timer {
+            ctr: String::new(),
+            current_cycle: 0,
+            value: 1,
+            next_value: None,
+            signial_strengh_over_time: vec![],
+        }
+    }
+    pub fn next_cycle(&mut self, command: Command) {
+        self.increment();
+        match command {
+            Command::Noop => {
+                self.next_value = None;
+            },
+            Command::AddX(value) => {
+                let _ = self.next_value.insert(value);
+                self.consume_next_value();
+            }
+        }
+    }
+
+    pub fn get_sum(&self) -> i64 {
+        self.signial_strengh_over_time.iter().sum()
+    }
+
+    pub fn get_ctr(&self) -> String {
+        self.ctr.clone()
+    }
+
+    fn increment(&mut self) {
+        self.current_cycle += 1;
+        self.add_signal_strengh();
+        self.draw();
+    }
+
+    fn consume_next_value(&mut self) {
+        if let Some(value) = self.next_value {
+            self.value += value;
+        }
+        self.increment();
+    }
+
+    fn is_sharp(&self) -> bool {
+        let spirit_start = self.value - 1; 
+        let spirit_end = self.value + 1;
+        let cycle = self.current_cycle as i64 % 40;
+        spirit_end >= cycle && cycle >= spirit_start
+    }
+
+    fn draw(&mut self) {
+        if self.is_sharp() {
+            self.ctr.push('#');
+        } else {
+            self.ctr.push('.');
+        }
+        if self.current_cycle % 40 == 0 {
+            self.ctr.push('\n');
+        }
+    }
+
+    fn is_observed(&self) -> bool {
+        let observed_cycle = [20, 60, 100, 140, 180, 220];
+        observed_cycle.contains(&self.current_cycle)
+    }
+
+    fn add_signal_strengh(&mut self) {
+        if self.is_observed() {
+            self.signial_strengh_over_time.push(self.current_cycle as i64 * self.value);
+            println!("cycle : {}, {:?}", self.current_cycle, self.signial_strengh_over_time);
+        }
+    }
+}
